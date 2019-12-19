f3500708ead36b009afcdb087e26228210a39937
Manuele Macchia <macchiamanuele@gmail.com>
2019-12-19 11:37:35 +0100

    implement intcode computer as a class

--- 09/09.py
+++ 09/09.py
@@ -13,76 +13,84 @@ STOP = 99
 with open('input.txt', 'r') as f:
     program = list(map(int, f.read().split(',')))
 
-def run(program):
-    p = program # Load program into memory
-    rb = 0 # Relative base
-    pos = 0 # Current position
+class Computer:
+    def __init__(self, program):
+        self.p = program
+        self.rb = 0
+        self.pos = 0
+        self.extra_memory_len = 10000
 
-    # Extend the program memory beyond the initial program
-    extra_memory_len = 10000
-    p = program + [0 for i in range(extra_memory_len)]
+    def run(self):
+        p = self.p # Load program into memory
+        rb = self.rb # Relative base
+        pos = self.pos # Current position
 
-    while p[pos] != STOP:
-        opcode = p[pos] % 100 # Extract the last two digits of the opcode
-        modes = int((p[pos] - opcode) / 100)
-        args = []
+        # Extend the program memory beyond the initial program
+        extra_memory_len = self.extra_memory_len
+        p = program + [0 for i in range(extra_memory_len)]
 
-        if opcode in [ADD, MUL, LT, EQ]:
-            modes = reversed(f"{modes:03d}")
-        
-        elif opcode in [JIT, JIF]:
-            modes = reversed(f"{modes:02d}")
-        
-        elif opcode in [IN, OUT, ARB]:
-            modes = [modes]
-        
-        arg_modes = [int(c) for c in modes]
+        while p[pos] != STOP:
+            opcode = p[pos] % 100 # Extract the last two digits of the opcode
+            modes = int((p[pos] - opcode) / 100)
+            args = []
 
-        for i in range(len(arg_modes)):
-            if arg_modes[i] == 0: # Position mode
-                args.append(p[pos+i+1])
-
-            elif arg_modes[i] == 1: # Immediate mode
-                args.append(pos+i+1)
+            if opcode in [ADD, MUL, LT, EQ]:
+                modes = reversed(f"{modes:03d}")
             
-            elif arg_modes[i] == 2: # Relative mode
-                args.append(p[pos+i+1] + rb)
-        
-        if opcode in [ADD, MUL, LT, EQ]:
-            if opcode == ADD:
-                p[args[2]] = p[args[0]] + p[args[1]]
+            elif opcode in [JIT, JIF]:
+                modes = reversed(f"{modes:02d}")
             
-            elif opcode == MUL:
-                p[args[2]] = p[args[0]] * p[args[1]]
+            elif opcode in [IN, OUT, ARB]:
+                modes = [modes]
             
-            elif opcode == LT:
-                p[args[2]] = 1 if p[args[0]] < p[args[1]] else 0
-            
-            elif opcode == EQ:
-                p[args[2]] = 1 if p[args[0]] == p[args[1]] else 0
-            
-            pos = pos + 4
+            arg_modes = [int(c) for c in modes]
 
-        elif opcode == JIT:
-            pos = p[args[1]] if p[args[0]] != 0 else pos + 3
-        
-        elif opcode == JIF:
-            pos = p[args[1]] if p[args[0]] == 0 else pos + 3
-        
-        elif opcode in [IN, OUT, ARB]:
-            if opcode == IN:
-                inp = input("Input: ")
-                p[args[0]] = int(inp)
+            for i in range(len(arg_modes)):
+                if arg_modes[i] == 0: # Position mode
+                    args.append(p[pos+i+1])
 
-            elif opcode == OUT:
-                print(f"Output: {p[args[0]]}")
+                elif arg_modes[i] == 1: # Immediate mode
+                    args.append(pos+i+1)
+                
+                elif arg_modes[i] == 2: # Relative mode
+                    args.append(p[pos+i+1] + rb)
             
-            elif opcode == ARB:
-                rb = rb + p[args[0]]
+            if opcode in [ADD, MUL, LT, EQ]:
+                if opcode == ADD:
+                    p[args[2]] = p[args[0]] + p[args[1]]
+                
+                elif opcode == MUL:
+                    p[args[2]] = p[args[0]] * p[args[1]]
+                
+                elif opcode == LT:
+                    p[args[2]] = 1 if p[args[0]] < p[args[1]] else 0
+                
+                elif opcode == EQ:
+                    p[args[2]] = 1 if p[args[0]] == p[args[1]] else 0
+                
+                pos = pos + 4
 
-            pos = pos + 2
-        
-        else:
-            print(f"Unknown opcode: {opcode}")
+            elif opcode == JIT:
+                pos = p[args[1]] if p[args[0]] != 0 else pos + 3
+            
+            elif opcode == JIF:
+                pos = p[args[1]] if p[args[0]] == 0 else pos + 3
+            
+            elif opcode in [IN, OUT, ARB]:
+                if opcode == IN:
+                    inp = input("Input: ")
+                    p[args[0]] = int(inp)
 
-run(program)
+                elif opcode == OUT:
+                    print(f"Output: {p[args[0]]}")
+                
+                elif opcode == ARB:
+                    rb = rb + p[args[0]]
+
+                pos = pos + 2
+            
+            else:
+                print(f"Unknown opcode: {opcode}")
+
+computer = Computer(program)
+computer.run()
