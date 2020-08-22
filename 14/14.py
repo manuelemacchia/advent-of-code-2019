f9dd076977cf1b1f5ea0a2023bf7f93ddb1bbe82
Manuele Macchia <macchiamanuele@gmail.com>
2020-08-22 21:58:56 +0200

    fourteenth day solution

--- 14/14.py
+++ 14/14.py
@@ -11,59 +11,85 @@ with open('14/input.txt', 'r') as f:
             'in': {r.split(" ")[1]: int(r.split(" ")[0]) for r in l[0].split(", ")}
         }
 
+# Part 1
+
 # Material that can be produced by ore directly
 primary = [k for k, v in reactions.items() if 'ORE' in v['in']]
 
-# Initialize to material required for 1 FUEL
-needs = reactions['FUEL']['in'].copy()
+def run(fuel):
+    # Initialize to material required
+    needs = reactions['FUEL']['in'].copy()
+    needs = {k: v*fuel for k, v in needs.items()}
 
+    leftovers = {}
+
+    while not all(need in primary for need in needs):
+        iter_needs = needs.copy()
+        current_needs = {}
+
+        for need, qty in iter_needs.items():
+            if need in primary:
+                continue
+
+            if need in leftovers:
+                if qty <= leftovers[need]: # if the qty needed is less than the qty in leftovers
+                    leftovers[need] -= qty
+                    continue
+                else:
+                    qty = qty - leftovers[need]
+                    leftovers[need] = 0
+
+            times = ceil(qty / reactions[need]['out'])
+
+            for req, req_qty in reactions[need]['in'].items():
+                if req in current_needs:
+                    current_needs[req] += req_qty * times
+                else:
+                    current_needs[req] = req_qty * times
+
+            leftovers[need] = reactions[need]['out'] * times - qty
+
+            needs.pop(need)
+
+        for need, qty in current_needs.items():
+            if need in leftovers:
+                if qty <= leftovers[need]:
+                    leftovers[need] -= qty
+                    continue
+                else:
+                    qty = qty - leftovers[need]
+                    leftovers[need] = 0
+            
+            if need in needs:
+                needs[need] += qty
+            else:
+                needs[need] = qty
+
+    ore = 0
+    for need, qty in needs.items():
+        times = ceil(qty / reactions[need]['out'])
+        ore += reactions[need]['in']['ORE'] * times
+
+    return ore
+
+print(run(1))
+
+# Part 2
+
+cargo = 1000000000000
 leftovers = {}
 
-while not all(need in primary for need in needs):
-    iter_needs = needs.copy()
-    current_needs = {}
+# By checking possible values of FUEL, we note that we are in the following FUEL range.
+# (NOTE: this may change for different inputs. Downside of brute-force approach!)
+print(run(3500000), run(4000000))
 
-    for need, qty in iter_needs.items():
-        if need in primary:
-            continue
+prev = 0
+cur = 0
+for fuel in range(3500000, 4000000):
+    cur = run(fuel)
 
-        if need in leftovers:
-            if qty <= leftovers[need]: # if the qty needed is less than the qty in leftovers
-                leftovers[need] -= qty
-                continue
-            else:
-                qty = qty - leftovers[need]
-                leftovers[need] = 0
+    if cur > cargo:
+        print(fuel-1, prev)
+        break
 
-        times = ceil(qty / reactions[need]['out'])
-
-        for req, req_qty in reactions[need]['in'].items():
-            if req in current_needs:
-                current_needs[req] += req_qty * times
-            else:
-                current_needs[req] = req_qty * times
-
-        leftovers[need] = reactions[need]['out'] * times - qty
-
-        needs.pop(need)
-
-    for need, qty in current_needs.items():
-        if need in leftovers:
-            if qty <= leftovers[need]:
-                leftovers[need] -= qty
-                continue
-            else:
-                qty = qty - leftovers[need]
-                leftovers[need] = 0
-        
-        if need in needs:
-            needs[need] += qty
-        else:
-            needs[need] = qty
-
-ore = 0
-for need, qty in needs.items():
-    times = ceil(qty / reactions[need]['out'])
-    ore += reactions[need]['in']['ORE'] * times
-
-print(ore)
+    prev = cur
